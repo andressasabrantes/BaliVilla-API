f748da8f8e64007f58c3da7acb9a15b77d805250
Andressa Abrantes <andressasabrantes@gmail.com>
2023-08-28 14:59:48 -0300

    Status Code in GetVilla EndPoint

--- BaliVilla_VillaAPI/Controllers/VillaAPIController.cs
+++ BaliVilla_VillaAPI/Controllers/VillaAPIController.cs
@@ -16,10 +16,20 @@ namespace BaliVilla_VillaAPI.Controllers
         }
 
         [HttpGet("{id:int}")]
-        public VillaDTO GetVilla(int id)
+        public ActionResult<VillaDTO> GetVilla(int id)
         {
-            return VillaStore.villaList.FirstOrDefault(u=>u.Id==id);
-        }
+            if (id == 0)
+            {
+                return BadRequest();
+            }
 
+            var villa = VillaStore.villaList.FirstOrDefault(u => u.Id == id);
+            if (villa == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(villa);
+        }
     }
 }
