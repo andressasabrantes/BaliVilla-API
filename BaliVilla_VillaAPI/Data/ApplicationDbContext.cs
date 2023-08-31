a794fc42f6e1974a56347534d9fcef9c3ec7548e
Andressa Abrantes <andressasabrantes@gmail.com>
2023-08-31 15:39:17 -0300

    Connection String and DbSet Entity

---
+++ BaliVilla_VillaAPI/Data/ApplicationDbContext.cs
@@ -0,0 +1,10 @@
+﻿using BaliVilla_VillaAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BaliVilla_VillaAPI.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
