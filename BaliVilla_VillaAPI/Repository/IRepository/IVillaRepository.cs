54f0b15b622f47a4baec2a00a8174732e21d0f80
Andressa Abrantes <andressasabrantes@gmail.com>
2023-09-04 16:10:51 -0300

    Adding Villa Repository Interface

---
+++ BaliVilla_VillaAPI/Repository/IRepository/IVillaRepository.cs
@@ -0,0 +1,14 @@
+﻿using BaliVilla_VillaAPI.Models;
+using System.Linq.Expressions;
+
+namespace BaliVilla_VillaAPI.Repository.IRepository
+{
+    public interface IVillaRepository
+    {
+        Task<List<Villa>> GetAll(Expression<Func<Villa>> filter = null);
+        Task<Villa> Get(Expression<Func<Villa>> filter = null, bool tracked = true);
+        Task Create(Villa entity);
+        Task Remove(Villa entity);
+        Task Save();
+    }
+}
