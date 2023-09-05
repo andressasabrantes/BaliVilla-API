d526d7c80cb14f838a7b5c210598cf869625d405
Andressa Abrantes <andressasabrantes@gmail.com>
2023-09-05 12:44:37 -0300

    Repository implemented in the APIController

--- BaliVilla_VillaAPI/Program.cs
+++ BaliVilla_VillaAPI/Program.cs
@@ -1,5 +1,7 @@
 using BaliVilla_VillaAPI;
 using BaliVilla_VillaAPI.Data;
+using BaliVilla_VillaAPI.Repository;
+using BaliVilla_VillaAPI.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,6 +10,7 @@ builder.Services.AddDbContext<ApplicationDbContext>(option => {
     option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection"));
 });
 
+builder.Services.AddScoped<IVillaRepository, VillaRepository>();
 builder.Services.AddAutoMapper(typeof(MappingConfig));
 builder.Services.AddControllers().AddNewtonsoftJson().AddXmlDataContractSerializerFormatters();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
