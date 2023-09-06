3f513e89d504e620c9c775d01057acd3ef5153fa
Andressa Abrantes <andressasabrantes@gmail.com>
2023-09-06 14:45:54 -0300

    AutoMapper configured

---
+++ BaliVilla_Web/MappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using BaliVilla_Web.Models.Dto;
+
+namespace BaliVilla_Web
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<VillaDTO, VillaCreateDTO>().ReverseMap();
+            CreateMap<VillaDTO, VillaUpdateDTO>().ReverseMap();
+
+            CreateMap<VillaNumberDTO, VillaNumberCreateDTO>().ReverseMap();
+            CreateMap<VillaNumberDTO, VillaNumberUpdateDTO>().ReverseMap();
+        }
+    }
+}
