7ab2b89da980f01f2cdfbcf1b0dd8265b8c60811
Andressa Abrantes <andressasabrantes@gmail.com>
2023-09-06 21:32:45 -0300

    Base Service for API

---
+++ BaliVilla_Web/Services/IServices/IBaseService.cs
@@ -0,0 +1,10 @@
+﻿using BaliVilla_Web.Models;
+
+namespace BaliVilla_Web.Services.IServices
+{
+    public interface IBaseService
+    {
+        APIResponse responseModel { get; set; }
+        Task<T> SendAsync<T>(APIRequest apiRequest);
+    }
+}
