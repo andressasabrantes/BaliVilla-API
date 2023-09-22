d7232d64c6318c24a926f2e30b2a35d888136006
Andressa Abrantes <andressasabrantes@gmail.com>
2023-09-21 22:54:58 -0300

    Update and Delete Action Methods for Villa Number

---
+++ BaliVilla_Web/Models/VM/VillaNumberDeleteVM.cs
@@ -0,0 +1,18 @@
+﻿using BaliVilla_Web.Models.Dto;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BaliVilla_Web.Models.VM
+{
+    public class VillaNumberDeleteVM
+    {
+        public VillaNumberDeleteVM()
+        {
+            VillaNumber = new VillaNumberDTO();
+        }
+
+        public VillaNumberDTO VillaNumber { get; set; }
+        [ValidateNever]
+        public IEnumerable<SelectListItem> VillaList { get; set; }
+    }
+}
