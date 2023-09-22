50bd84db62f3560ee40d9af486e3b30f987da7c5
Andressa Abrantes <andressasabrantes@gmail.com>
2023-09-21 22:06:07 -0300

    Special Validation and Base Service Update

--- BaliVilla_Web/Services/BaseService.cs
+++ BaliVilla_Web/Services/BaseService.cs
@@ -2,6 +2,7 @@
 using BaliVilla_Web.Models;
 using BaliVilla_Web.Services.IServices;
 using Newtonsoft.Json;
+using System;
 using System.Text;
 
 namespace BaliVilla_Web.Services
@@ -51,8 +52,27 @@ namespace BaliVilla_Web.Services
                 apiResponse = await client.SendAsync(message);
 
                 var apiContent = await apiResponse.Content.ReadAsStringAsync();
+                try
+                {
+                    APIResponse ApiResponse = JsonConvert.DeserializeObject<APIResponse>(apiContent);
+                    if (apiResponse.StatusCode == System.Net.HttpStatusCode.BadRequest || apiResponse.StatusCode == System.Net.HttpStatusCode.NotFound)
+                    {
+                        ApiResponse.StatusCode = System.Net.HttpStatusCode.BadRequest;
+                        ApiResponse.IsSuccess = false;
+                        var res = JsonConvert.SerializeObject(ApiResponse);
+                        var returnObj = JsonConvert.DeserializeObject<T>(res);
+                        return returnObj;
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    var exceptionResponse = JsonConvert.DeserializeObject<T>(apiContent);
+                    return exceptionResponse;
+                }
                 var APIResponse = JsonConvert.DeserializeObject<T>(apiContent);
                 return APIResponse;
+
             }
             catch(Exception e)
             {
