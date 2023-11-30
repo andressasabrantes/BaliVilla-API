436d2507975b9a292b7381c78cd3940d98aabb33
Andressa Abrantes <andressasabrantes@gmail.com>
2023-11-30 12:22:00 -0300

    Adding Action Methods in the Auth Controller

---
+++ BaliVilla_Web/Controllers/AuthController.cs
@@ -0,0 +1,56 @@
+﻿using BaliVilla_Web.Models;
+using BaliVilla_Web.Models.Dto;
+using BaliVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BaliVilla_Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        public IActionResult Login()
+        {
+            LoginRequestDTO obj = new();
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginRequestDTO obj)
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegistrationRequestDTO obj)
+        {
+            APIResponse result = await _authService.RegisterAsync<APIResponse>(obj);
+            if (result != null && result.IsSuccess)
+            {
+                return RedirectToAction("Login");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            return View();
+        }
+
+        public IActionResult AccessDenied()
+        {
+            return View();
+        }
+    }
+}
