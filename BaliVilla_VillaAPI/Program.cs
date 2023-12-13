47e5dc07c3599071d06575c4f27401d495999aa8
Andressa Abrantes <andressasabrantes@gmail.com>
2023-12-13 16:09:33 -0300

    Swagger Documentation for v1

--- BaliVilla_VillaAPI/Program.cs
+++ BaliVilla_VillaAPI/Program.cs
@@ -81,6 +81,18 @@ builder.Services.AddSwaggerGen(options =>
             new List<string>()
         }
     }); ;
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1.0",
+        Title = "Bali Villa",
+        Description = "API to manage the Villa",
+        TermsOfService = new Uri("https://example.com/terms"),
+        Contact = new OpenApiContact
+        {
+            Name = "Andressa Abrantes",
+            Url = new Uri("https://github.com/andressasabrantes")
+        }
+    });
 });
 
 var app = builder.Build();
@@ -89,7 +101,10 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Bali_VillaV1");
+    });
 }
 
 app.UseHttpsRedirection();
